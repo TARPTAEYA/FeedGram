e0e2a2b79ec31e7dbf13f1bf4eb3026ecbbc512a
Anmol Gautam <anmolgautam@Anmols-MacBook-Pro.local>
2020-01-18 19:53:32 +0530

    Updates

---
+++ precache-manifest.5dc7708cb0c736b0b656bad7fc0def47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a96888ddcee4be4bc78f4b9b77d2255",
+    "url": "/feedgram/index.html"
+  },
+  {
+    "revision": "c612856b242e762e1296",
+    "url": "/feedgram/static/css/main.74e3276e.chunk.css"
+  },
+  {
+    "revision": "91c502cf3ada75e5e3a4",
+    "url": "/feedgram/static/js/2.f83872da.chunk.js"
+  },
+  {
+    "revision": "45a8d1d01f90d76fa6709dbdba550019",
+    "url": "/feedgram/static/js/2.f83872da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c612856b242e762e1296",
+    "url": "/feedgram/static/js/main.3d7f6594.chunk.js"
+  },
+  {
+    "revision": "159a607f51db9ae8aecd",
+    "url": "/feedgram/static/js/runtime-main.da3d5007.js"
+  }
+]);
